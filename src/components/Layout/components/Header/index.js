6b9b6caa5398c7f92878d03222816376655f0d9c
shopeeTest <volemenn@gmail.com>
2022-08-20 15:30:10 +0700

    update

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,4 +1,6 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss'
 import images from '~/assest/images';
 const cx = classNames.bind(styles)
@@ -6,12 +8,19 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <div className={cx('logo')}>
-                    <img src={images.logo} alt="tiktok"/>
-                </div>
+                <img src={images.logo} alt="tiktok"/>
                 <div className={cx('search')}>
                     <input spellCheck={false} placeholder='Tìm kiếm tài khoản và video'/>
-                    <button></button>
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark}/>
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner}/>
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                    </button>
+                </div>
+                <div className={cx('actions')}>
+
                 </div>
             </div>
         </header>
